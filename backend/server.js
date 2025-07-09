1074ef7d527a5f1b01dac25c697780c69e47a673
Harish K <143332050+Harish-Kuppusamy@users.noreply.github.com>
2025-07-09 15:25:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "admin" },
+  role: { type: String, default: "user" },
 });
 
 const User = mongoose.model("user", userSchema);
